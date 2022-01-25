c7bd3aea035d20f918dbaa344f2fe2074d472d21
Ting Keong Sik <34431225+TingKeongSik@users.noreply.github.com>
2022-01-26 03:43:39 +0800

    HTML

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,19 +1,6 @@
-$(document).ready(function(){
-	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-		$(this).toggleClass('open');
-		$('#navbar-list').css('width',$(this).hasClass('open')?"100%":0)
-	});
-
-	$('.first-button').on('click', function () {
-  
-	  $('.animated-icon1').toggleClass('open');
-	});
-	$('.second-button').on('click', function () {
-  
-	  $('.animated-icon2').toggleClass('open');
-	});
-	$('.third-button').on('click', function () {
-  
-	  $('.animated-icon3').toggleClass('open');
-	});
+$(document).ready(function () {
+  $("#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4").click(function () {
+    $(this).toggleClass("open");
+    $("#navbar-list").css("width", $(this).hasClass("open") ? "100%" : 0);
   });
+});
